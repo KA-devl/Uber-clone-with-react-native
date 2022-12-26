ac5c92a762c2f08667cfdb3915f720fc71505582
Anes Khadiri <anes1999@live.ca>
2022-12-25 23:43:49 -0500

    Finished implementation of NavOptions

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,7 @@
 import { Text, View, FlatList, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
+import { Icon } from "@rneui/base";
 
 const data = [
   {
@@ -23,17 +24,16 @@ const NavOptions = () => {
       keyExtractor={(item) => item.id}
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity
-        style={tw `p-2 pl-6 pb-8 pt-5 bg-gray-200 m-2 w-40 `}>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-5 bg-gray-200 m-2 w-40 `}>
           <View>
             <Image
-            style={{width : 120, height : 120, resizeMode: "contain" }}
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
               source={{
                 uri: item.image,
               }}
             />
-            <Text style={tw `mt-2 text-lg font-semibold`}>{item.title}</Text>
-            
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon style={tw `p-2 bg-black rounded-full w-10 mt-4`} name="arrowright" color="white" type="antdesign" />
           </View>
         </TouchableOpacity>
       )}
